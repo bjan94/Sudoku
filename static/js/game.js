21c9022c0ff010443c45e57654edb3b96e009cf8
Byung Joon An <bjan94@gmail.com>
2017-12-10 16:45:23 -0500

    board html

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,19 @@
+var selected;
 $(document).ready(function() {
-   console.log("ready");
+   selected = undefined;
+   $(".table-inner td").on('click', function() {
+      toggle($(this));
+   });
 });
+
+function toggle(element) {
+   if (selected != undefined && selected != element) {
+      untoggle(selected);
+   }
+   selected = element;
+   element.css('background-color', '#3D9EFD');
+}
+
+function untoggle(element) {
+   element.css('background-color', 'white');
+}
